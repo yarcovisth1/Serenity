f5ad932e9c34602c89be062ca326da7070a71beb
Trinitron <yarcovisth1@gmail.com>
2024-10-30 00:00:09 -0500

    Se agregan nuevos test

--- src/test/java/starter/math/WhenMultiplyingNumbers.java
+++ src/test/java/starter/math/WhenMultiplyingNumbers.java
@@ -5,40 +5,42 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import starter.steps.MathWizSteps;
+
 import java.util.Random;
 
-@RunWith(SerenityRunner.class)
-@Narrative(text={"La division es una operación clave en matemáticas."})
-public class WhenAddingNumbers {
 
+@RunWith(SerenityRunner.class)
+@Narrative(text={"La multiplicación es una operación clave en matemáticas."})
+public class WhenMultiplyingNumbers {
 
     @Steps
     MathWizSteps miguel;
 
     private Random random = new Random();
     private int[] numeros = {1, 2, 3, 4, 5};
+
     @Test
-    public void suma() {
+    public void multiplicacion(){
         // Given
-        miguel.tiene(1);
+        miguel.tiene(3);
         // When
-        miguel.suma(2);
+        miguel.multiplica(4);
         // Then
-        miguel.deberiaTener(3);
+        miguel.resultadoFactor(12);
     }
 
     @Test
-    public void sumaAleatoria() {
+    public void multiplicacionAleatoria() {
         // Given
         int numeroAleatorio1 = numeros[random.nextInt(numeros.length)]; // Selecciona un número aleatorio del vector
         int numeroAleatorio2 = numeros[random.nextInt(numeros.length)]; // Selecciona otro número aleatorio del vector
 
         // When
         miguel.tiene(numeroAleatorio1); // Asigna el primer número
-        miguel.suma(numeroAleatorio2); // Multiplica por el segundo número
+        miguel.multiplica(numeroAleatorio2); // Multiplica por el segundo número
 
         // Then
-        int resultadoEsperado = numeroAleatorio1 + numeroAleatorio2; // Calcula el resultado esperado
-        miguel.deberiaTener(resultadoEsperado); // Verifica el resultado
+        int resultadoEsperado = numeroAleatorio1 * numeroAleatorio2; // Calcula el resultado esperado
+        miguel.resultadoFactor(resultadoEsperado); // Verifica el resultado
     }
 }
